58d75c59330d8c598abbc6fdfbc5266cfe098989
Andreas Herzog <andreas.herzog@point-of-media.com>
2015-08-14 19:43:35 +0200

    add debug module to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var debug = require('debug')('realtime-hub:server');
 
 var io = require('./ioRouter');
 var app = express();
@@ -9,12 +10,17 @@ app.use(express.static('public'));
 
 app.set('port', 3000);
 
+app.use(function (req, res, next){
+    debug(req.method + ' ' + req.url);
+    next();
+});
+
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/public/client.html');
 });
 
 server = app.listen(app.get('port'), function() {
-    console.log('Express server running on port ' + app.get('port'));
+    debug('Express server running on port ' + app.get('port'));
 });
 
 io.init(server);
