a14c01e207f7d2e5ebbc8b961b3817fcd8b50205
Andreas Herzog <andreas.herzog@point-of-media.com>
2015-08-14 23:01:56 +0200

    add debug logging everywheren, add error handler for sockets

--- ioRouter.js
+++ ioRouter.js
@@ -1,17 +1,20 @@
-var ioRouter = {};
 
 var socketIo = require('socket.io');
+var debug = require('debug')('realtime-hub:ioRouter');
 
 var twitterIoHandler = require('./lib/handler/twitterIo');
 var countIoHandler = require('./lib/handler/countIo');
+var ioUtils = require('./lib/util/io');
 
+var ioRouter = {};
 module.exports = ioRouter;
 
 ioRouter.init = function(server){
     var io = socketIo(server);
     io.on('connection', function (socket) {
-        console.log('client ' + socket.id + ' connected, to start streaming press button...');
-        socket.on('countUp', countIoHandler.countUp);
+        debug('client ' + socket.id + ' connected on socket');
+        socket.on('error', ioUtils.handleSocketError);
+        socket.on('countUp', countIoHandler.countUp.bind(null, socket));
         socket.on('Stream:start', twitterIoHandler.startStream.bind(null, socket));
         socket.on('Stream:stop', twitterIoHandler.stopStream.bind(null, socket));
         socket.on('Stream:live', twitterIoHandler.liveStream.bind(null, socket));
